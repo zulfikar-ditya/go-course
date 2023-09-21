b92ff09da48f9a713b9407751239853a945e95b2
Zulfikar Ditya <zulfikarditya.work@gmail.com>
2023-09-21 12:50:33 +0700

    Pgsql

---
+++ handleUser.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/zulfikar-ditya/go-course/internal/database"
+)
+
+func (APIConfig *APIConfig) handleCreateNewUser(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	type person struct {
+		Name string `json:"name"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := person{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		responseWithError(w, http.StatusBadRequest, fmt.Sprintf("Invalid request payload", err))
+		return
+	}
+
+	user, err := APIConfig.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID: uuid.New(),
+		Name: params.Name,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+
+	if err != nil {
+		responseWithError(w, http.StatusInternalServerError, fmt.Sprintf("Failed to create user: %s", err))
+		return
+	}
+
+	responseWithJson(w, http.StatusOK, user)
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,13 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	"github.com/zulfikar-ditya/go-course/internal/database"
+	_"github.com/lib/pq"
 )
+
+type APIConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	getEnv := godotenv.Load()
@@ -24,7 +31,21 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	fmt.Println("Running development server on port " + portString + "...")
+	dbUrl := os.Getenv("DB_URL")
+
+	if dbUrl == "" {
+		log.Fatal("$DB_URL must be set")
+	}
+
+	conn, err := sql.Open("postgres", dbUrl)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	apiConfig := APIConfig{
+		DB: database.New(conn),
+	}
 
 	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
@@ -39,16 +60,18 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/test", handlerReadiness)
 	v1Router.Get("/error", handlerErr)
-
+	v1Router.Post("/users", apiConfig.handleCreateNewUser)
+	
 	router.Mount("/v1", v1Router)
-
+	
 	server := &http.Server{
 		Handler: router,
 		Addr: ":" + portString,
 	}
-
-	err := server.ListenAndServe()
-
+	
+	fmt.Println("Running development server on port " + portString + "...")
+	err = server.ListenAndServe()
+	
 	if err != nil {
 		log.Fatal(err)
 	}
